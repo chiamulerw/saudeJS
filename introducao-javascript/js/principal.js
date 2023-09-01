68375a0ce266a798c999c71d2c3aaf0ccdb076db
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-09-01 09:08:18 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -12,5 +12,22 @@ var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso < 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+if(altura < 0 || altura >= 3.00) {
+    console.log("ALtura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!s"
+}
+  
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
