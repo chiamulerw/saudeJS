4a2ece0eb11ddcf9d6abf6ed870a0103921b73c4
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-09-13 08:21:19 -0300

    Add files via upload

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -39,6 +39,18 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(){
-    console.log("oi eu fui clicado")
-});
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
+})
