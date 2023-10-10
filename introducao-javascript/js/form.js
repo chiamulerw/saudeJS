6d0ec72baad705c72fd55c469c67afc5a1c9974a
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-10-10 11:47:39 -0300

    Add files via upload

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -6,33 +6,17 @@ botaoAdicionar.addEventListener("click", function(event){
 //extraindo informacoes do paciente do form
    var paciente = obtemPacienteDoFormulario(form);
 
-   console.log(paciente);
-  
+   console.log(paciente)
 
 //cria a tr e a td do paciente
-    var pacienteTr = document.createElement("tr");
+   var pacienteTr = montaTr(paciente);
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-//adicionando paicente na tabeela
+//adicionando paicente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
+
+    form.reset();
 });
 
 function obtemPacienteDoFormulario(form){
@@ -45,4 +29,25 @@ function obtemPacienteDoFormulario(form){
         imc: calculaImc(form.peso.value, form.altura.value)
     }
     return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
 }
