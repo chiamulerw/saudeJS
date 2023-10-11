b5e2144268eb88107dcc584429ff6d713a663e54
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-10-11 07:54:17 -0300

    Add files via upload

---
+++ introducao-javascript/saude_js-main/javascript/form.js
@@ -0,0 +1,67 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+var form = document.querySelector("#form-adiciona");
+//extraindo informacoes do paciente do form
+var paciente = obtemPacienteDoFormulario(form);
+
+//cria a tr e a td do paciente
+var pacienteTr = montaTr(paciente);
+
+var erro = validaPaciente(paciente)
+
+if(erro.length > 0 ){
+    var mensagemErro = document.querySelector("#mensagem-erro");
+    mensagemErro.textContent = erro;
+    return;
+}
+   
+//adicionando paciente na tabela
+var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+    form.reset();
+});
+
+function obtemPacienteDoFormulario(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+    return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado, classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+
+    return td;
+}
+
+function validaPaciente(paciente){
+    if(validaPeso(paciente.peso)){
+        return "";
+    }else{
+        return "O peso é inválido!";
+    }
+}
