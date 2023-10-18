f53bba64b4eb62980ccbd498bfe4eccc3f00ae22
Ana Chiamulera <127249227+chiamulerw@users.noreply.github.com>
2023-10-18 08:20:25 -0300

    Add files via upload

--- saude_js-main/javascript/form.js
+++ saude_js-main/javascript/form.js
@@ -22,10 +22,13 @@ var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
 });
 
 function exibeMensagemErro(erros){
     var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
     erros.forEach(function(erro){
         var li = document.createElement("li");
         li.textContent = erro;
@@ -70,21 +73,26 @@ function validaPaciente(paciente){
 
     var erros = []; 
 
-    if(paciente.nome.length == 0)
-    erros.push("O nome não pode ser em branco");
-
     if(!validaPeso(paciente.peso))
         erros.push("Peso é inválido");
-    
 
     if (!validaAltura(paciente.altura))
         erros.push("Altura é inválida");
 
-        if(paciente.gordura.length == 0)
+    if(paciente.nome.length == 0)
+        erros.push("O nome não pode ser em branco");
+
+    if(paciente.gordura.length == 0)
         erros.push("A gordura não pode ser em branco");
+
+    if(paciente.peso.length == 0)
+        erros.push("O peso não pode ser em branco");
+
+    if(paciente.altura.length == 0)
+        erros.push("A altura não pode ser em branco");
 
     return erros;
 }
 
 
-// parei no minuto 10 do video
+// parei no minuto do video
